e54c772848bf243da3e0bc082eeca98cc936c3d0
den9995 <den9995@gmail.com>
2023-03-12 08:24:18 +0300

    Заготовка под входную функцию

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    //println!("Hello, world!");
+	let f = ProvidedFunc { x: [0.1,0.2].to_vec(), s: "a"};
+	println!("val {}",f.compute());
 }
+pub struct ProvidedFunc<'a> {
+	x: Vec<f64>,
+	s: &'a str ,
+}
+impl ProvidedFunc<'_> {
+	pub fn compute(&self) -> f64{
+		return self.x.iter().sum();
+	}
+}
+

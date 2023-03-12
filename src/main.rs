92500de3e55f0c1e86d43422122ddf957087f199
den9995 <den9995@gmail.com>
2023-03-12 08:54:06 +0300

    заготовка под юнит тесты

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
 fn main() {
     //println!("Hello, world!");
-	let f = ProvidedFunc { x: [0.1,0.2].to_vec(), s: "a"};
-	println!("val {}",f.compute());
+	let f = ProvidedFunc { s: "x1 + x2"};
+	println!("val {}",f.compute([0.1,0.2].to_vec()));
 }
 pub struct ProvidedFunc<'a> {
-	x: Vec<f64>,
 	s: &'a str ,
 }
 impl ProvidedFunc<'_> {
-	pub fn compute(&self) -> f64{
-		return self.x.iter().sum();
+	pub fn compute(&self, x: Vec<f64>) -> f64{
+		return x.iter().sum();
+	}
+}
+#[cfg(test)]
+mod tests {
+	// Note this useful idiom: importing names from outer (for mod tests) scope.
+	use super::*;
+	fn test_add_func() {
+		let func = ProvidedFunc { s:"x1 + x2" };
+		assert_eq!(func.compute([1.0, 2.0].to_vec()), 3.0);
 	}
 }
 

ba7e4f86eda32e43975855380b5ac11317752a17
den9995 <den9995@gmail.com>
2023-03-24 19:47:43 +0300

    добавил отражение

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,14 @@
 use random::Source;
 use std::cmp::Ordering;
+use std::time::{SystemTime, UNIX_EPOCH};
+
 
 fn main() {
    let string =  "x1 + x2";
    let func = ProvidedFunc { s: "x1 2 pow x2 2 pow +"};
    func.nelder_mid(1.0,0.5,2.0);
-   //let mut vt = vec![0.0;func.count_x()];
-   //std::mem::swap(&mut vec[0], &mut vt);
-   //std::mem::swap(&mut vec[1], &mut vt);
-   //std::mem::swap(&mut vec[0], &mut vt);
-   //println!("vec {:#?}",vec.iter().map(|row| func.compute(row)));
-   //println!("vec {:#?}",vec);
    
 }
-//pub struct xval {
-//   x: Vec<f64>,
-//}
 pub struct ProvidedFunc<'a> {
    s: &'a str ,
 }
@@ -27,31 +20,66 @@ impl ProvidedFunc<'_> {
       return 2;
    }
    pub fn nelder_mid(&self, a: f64, b: f64, g: f64) {
+      let maxSteps = 1000000;
+      let mut stepsn = maxSteps;
       let mut vec = vec![vec![0.0; self.count_x()]; self.count_x()+1];
-      let mut rng = random::default(0);
-      vec = vec.iter().map(|row| row.iter().map(|_| rng.read::<f64>()).collect()).collect();
-      for n in 0..1 {
+      let start = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).expect("REASON").as_nanos().try_into().unwrap();
+      //let start = 15;
+      let mut rng = random::default(start);
+      println!("rng seed {}, ", start);
+      for j in 0..(self.count_x()) {
+         vec[0][j]=(rng.read::<f64>()-0.5)*2.0;
+      }
+      for i in 1..(self.count_x()+1) {
+         for j in 0..(self.count_x()) {
+            vec[i][j]=vec[i-1][j]+(rng.read::<f64>()-0.5)*2.0;
+         }
+      }
+      //println!("vec {:#?}, ", vec);
+      //println!("values {:#?}",vec.iter().map(|row| self.compute(row)).collect::<Vec<f64>>());
+      for n in 0..maxSteps{
          vec.sort_by(|a, b| self.compute(a).partial_cmp(&self.compute(b)).unwrap_or(Ordering::Equal));
+         let diff = self.compute(&vec[self.count_x()])-self.compute(&vec[0]);
+         if diff < 0.0000001 {
+            stepsn=n;
+            break;
+         }
          let mut xc = vec![0.0; self.count_x()];
-         for v in &vec{
+         for v in (&vec).into_iter().take(self.count_x()) {
             for (i, x) in v.into_iter().enumerate() {
-               xc[i] += x/(self.count_x()+1) as f64;
+               xc[i] += x/(self.count_x()) as f64;
             }
          }
-         println!("xc {:#?}",xc);
-         println!("vec {:#?}, values {:#?}, {} {} {}",vec,vec.iter().map(|row| self.compute(row)).collect::<Vec<f64>>(),a,b,g);
-         let mut xr = vec![0.50; self.count_x()];
-         self.expand(&mut vec, &xc,&xr,g);
-         println!("vec {:#?}, values {:#?}, {} {} {}",vec,vec.iter().map(|row| self.compute(row)).collect::<Vec<f64>>(),a,b,g);
+         self.reflect(&mut vec, &xc,a,b,g);
+      }
+      
+      vec.sort_by(|a, b| self.compute(a).partial_cmp(&self.compute(b)).unwrap_or(Ordering::Equal));
+      println!("vec {:#?}, {}", vec, stepsn);
+      println!("values {:#?}",vec.iter().map(|row| self.compute(row)).collect::<Vec<f64>>());
+   }
+   fn reflect(&self, vec: &mut Vec<Vec<f64>>, xc: &Vec<f64>, a:f64, b:f64, g:f64)  {
+      let mut xr = vec![0.0; self.count_x()];
+      for i in 0..self.count_x() {
+         xr[i] = (1.0+a)*xc[i]+a*vec[vec.len()-1][i];
+      }
+      let worst = vec.len()-1;
+      let good =  vec.len()-2;
+      if self.compute(&xr) <= self.compute(&vec[0]) {
+         self.expand(vec, &xc, &xr,g);
+      } else if self.compute(&xr) <= self.compute(&vec[good]) {
+         for i in 0..self.count_x() {
+            vec[worst][i]=xr[i];
+         }
+         self.shrink(vec, &xc,b);
+      } else if self.compute(&xr) <= self.compute(&vec[worst]) {
+         for i in 0..self.count_x() {
+            vec[worst][i]=xr[i];
+         }
+         self.shrink(vec, &xc,b);
+      } else { 
+         self.shrink(vec, &xc,b);
       }
    }
-//   fn reflect(&self, xw: &Vec<f64>, xc: &Vec<f64>, a:f64) -> Vec<f64> {
-//      let mut xr = vec![0.0; self.count_x()];
-//      for i in 0..self.count_x() {
-//         xr[i] = (1.0+a)*xc[i]+a*xw[i];
-//      }
-//      return xr;
-//   }
    fn shrink(&self, vec: &mut Vec<Vec<f64>>, xc: &Vec<f64>, b:f64)  {
       let mut xs = vec![0.0; self.count_x()];
       for i in 0..self.count_x() {
@@ -86,7 +114,6 @@ impl ProvidedFunc<'_> {
          }
          
       }
-      
    }
 }
 #[cfg(test)]
